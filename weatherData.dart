cb3e72b9d3b7d1cb466895a0eb958cc9a33fd7bb
Ananya Agrawal <77062539+ananya704@users.noreply.github.com>
2021-06-20 19:01:40 +0530

    Update weatherData.dart

--- weatherData.dart
+++ weatherData.dart
@@ -3,38 +3,33 @@ import 'package:intl/intl.dart';
 import 'dart:convert';
 
 class WeatherData {
-  final String name;
+  String name;
   String stat;
   String icon;
   double temp;
   String time;
-  bool isDayTime;
+  // bool isDayTime;
 
   WeatherData(this.name);
 
   Future<void> getTime() async {
     // make a request
-    http.Response response = await http.get(Uri.https(
-        'https://api.openweathermap.org/data/2.5/weather?q=$name',
-        '&appid=c2397a293bad6bf5af8760b6e4207dfb'));
+    print('abc');
+    http.Response response = await http.get(Uri.https('api.openweathermap.org',
+        '/data/2.5/weather?q=$name&appid=c2397a293bad6bf5af8760b6e4207dfb'));
+    print('kyuuuu');
+    print(response.body);
     Map data = jsonDecode(response.body);
 
-    // get properties from json
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1, 3);
-    String offset_min = data['utc_offset'].substring(4, 6);
-
-    // create DateTime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(
-        Duration(hours: int.parse(offset), minutes: int.parse(offset_min)));
-
-    // set time
-
-    isDayTime = now.hour < 20 && now.hour > 5;
-    time = DateFormat.jm().format(now);
+    time =
+        new DateTime.fromMillisecondsSinceEpoch(data['dt'] * 1000, isUtc: false)
+            .toString();
     temp = data['main']['temp'].toDouble();
     stat = data['weather'][0]['main'];
     icon = data['weather'][0]['icon'];
+    print(time);
+    print(temp);
+    print(stat);
+    print(icon);
   }
 }
