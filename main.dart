504d67928acf884630c67bd6d41cdf246a3d973f
Ananya Agrawal <77062539+ananya704@users.noreply.github.com>
2021-06-21 00:28:57 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import './screens/loading.dart';
+
 import './screens/home_screen.dart';
 
 void main() => runApp(MyApp());
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Home(),
       routes: {
-        LoadingScreen.routeName: (ctx) => LoadingScreen(),
+        
         // Home.routeName: (ctx) => Home(),
       },
     );
