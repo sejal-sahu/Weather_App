482fe5f53abebf0f35867edf3b188b7e05948af5
Sejal Sahu <82333584+sejal-sahu@users.noreply.github.com>
2021-06-22 14:46:15 +0530

    Update home_screen.dart

--- home_screen.dart
+++ home_screen.dart
@@ -1,79 +1,216 @@
-import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'weatherData.dart';
 import 'package:intl/intl.dart';
-import 'dart:convert';
 
-class WeatherData {
-  String name;
-  String stat;
-  String icon;
-  double temp;
-  String time;
+class Home extends StatefulWidget {
+  static const routeName = '/home';
+  @override
+  _HomeState createState() => _HomeState();
+}
 
-  double feel;
-  double speed;
-  double pressure; //mbar
-  int humidity;
-  int rise;
-  int set;
-
-
-  // bool isDayTime;
-
-  WeatherData({this.time, this.name, this.temp, this.stat, this.icon}); //this.name
-
-  Future<void> getTime() async {
-    // make a request
-    // print('abc');
-    // http.Response response = await http.get(Uri.https('api.openweathermap.org',
-    //     '/data/2.5/weather?q=$name&appid=c2397a293bad6bf5af8760b6e4207dfb'));
-    http.Response response = await http.get(Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?q=$name&appid=c2397a293bad6bf5af8760b6e4207dfb'));
-    // http.Response response = await http.get(Uri.http(
-    //     'http://api.openweathermap.org',
-    //     '/data/2.5/weather?q=$name&appid=c2397a293bad6bf5af8760b6e4207dfb'));
-
-     print('kyuuuu');
-    print(response.body);
-    Map data = jsonDecode(response.body);
-     /*WeatherData({this.date, this.name, this.temp, this.main, this.icon});
-
-  factory WeatherData.fromJson(Map<String, dynamic> json) {
-    return WeatherData(
-      date: new DateTime.fromMillisecondsSinceEpoch(json['dt'] * 1000, isUtc: false),
-      name: json['name'],
-      temp: json['main']['temp'].toDouble(),
-      main: json['weather'][0]['main'],
-      icon: json['weather'][0]['icon'],
-    );
+class _HomeState extends State<Home> {
+  WeatherData data = new WeatherData(name: "delhi");
+  final _text = TextEditingController();
+  void getData() async {
+    print('kk');
+    await data.getTime();
+    print('jj');
+    setState(() {});
   }
-  */
 
-    time =
-        new DateTime.fromMillisecondsSinceEpoch(data['dt'] * 1000, isUtc: false)
-            .toString();
-             name= data['name'];
+  void initState() {
+    //super.initState();
+    data.getTime();
+    getData();
+  }
+
+  void handleClick(String value) {
+    switch (value) {
+      case 'Logout':
+        break;
+      case 'Settings':
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(data.feel); print('why');
+    print(data.temp);
+    print(data.stat);
+    print(data.icon);
+     print(data.pressure);
+    print(data.humidity);
+    print(data.speed);
+
+    var time1 = DateTime.parse(data.time);
+    var timezone = DateFormat('yyyy-MM-dd kk:mm:ss').format(time1);
+    print(timezone);
+    var t1 = data.temp;
+    double temper = (t1 - 273.15);
+    var tempera = temper.toInt();
+    //var tempera = double.parse((temper).toStringAsFixed(2));
+    var feels;
     
-    temp = data['main']['temp'].toDouble();
-    stat = data['weather'][0]['main'];
-    icon = data['weather'][0]['icon'];
+double windfeels = data.feel - 273.15;
+feels = windfeels.toInt();
+ //feels= double.parse((windfeels).toStringAsFixed(0));
+ var pressUre = data.pressure.toInt();
 
-    feel = data['main']['feels_like'].toDouble();
-    speed = data['wind']['speed'];
-    pressure = data['main']['pressure'].toDouble() ;
-    humidity = data['main']['humidity'].toInt() ;
-    rise = data['sys']['sunrise'];
-    set = data['sys']['sunset'];
-    print(feel);
-    print(name);
-    print(temp);
-    print(stat);
-    print('huehuehue');
-     print(rise);
-     print(set);
-    print(icon);
-     print(pressure);
-    print(humidity);
-    print(speed);
-  
+
+  var sr = DateTime.fromMillisecondsSinceEpoch(data.rise * 1000);
+  var sunrise = DateFormat('HH:mm').format(sr);
+var ss = DateTime.fromMillisecondsSinceEpoch(data.set* 1000);
+var sunset = DateFormat('HH:mm').format(ss);
+
+
+print(sunrise);
+print('why same set');
+print(sunset);
+print('sunrise');
+
+    final Map<String, AssetImage> images = {
+      "Clouds": AssetImage("assets/images/cloudy.jpg"),
+      "Clear": AssetImage("assets/images/clear.jpg"),
+      "Haze": AssetImage("assets/images/haze.jpg"),
+      "Mist": AssetImage("assets/images/mist.jpg"),
+    };
+    // data = data.isNotEmpty ? data : ModalRoute.of(context).settings.arguments;
+    //return Image(image: AssetImage('graphics/background.png'));
+    return Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+            title: Text('Weather app'),
+            centerTitle: true,
+            backgroundColor: Colors.blueGrey,
+            leading: Icon(
+              Icons.add_location_alt_outlined,
+            )),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: (data.stat.toString()) == null
+                  ? images["Clear"]
+                  : images[data.stat.toString()],
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Container(
+                margin:
+                    EdgeInsets.only(left: 20, top: 20, right: 20, bottom: 50),
+                padding:
+                    EdgeInsets.only(left: 5, top: 5, right: 20, bottom: 00),
+                height: 50,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(3),
+                      topRight: Radius.circular(3),
+                      bottomLeft: Radius.circular(3),
+                      bottomRight: Radius.circular(3)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.3),
+                      spreadRadius: 3,
+                      blurRadius: 5,
+                      offset: Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    IconButton(
+                      icon: new Icon(Icons.search),
+                      onPressed: () {
+                        setState(() {
+                          data.name = _text.text;
+
+                          data.getTime();
+                        });
+                      },
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                        child: TextField(
+                      controller: _text,
+
+                      decoration:
+                          InputDecoration.collapsed(hintText: "Enter City"),
+
+                      // onSubmitted: (String city) =>
+                      //     {model.refreshWeather(city, context)})),
+                    ))
+                  ],
+                ),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(14.0),
+                    child: Column(
+                      children: <Widget>[
+                        Text('${data.name.toString()}',
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 30)),
+                        Text('${tempera.toString()}°C',
+                            style: new TextStyle(
+                                color: Colors.white,
+                                fontSize: 64.0,
+                                fontWeight: FontWeight.bold)),
+                        Text('${data.stat.toString()}',
+                            style: new TextStyle(
+                                color: Colors.white, fontSize: 40.0)),
+                        Image.network(
+                            'https://openweathermap.org/img/w/${data.icon}.png'),
+                        Text('${timezone.toString()}',
+                            style: new TextStyle(color: Colors.white)),
+                        Container(
+                          alignment: Alignment.bottomCenter ,
+                          child: Column(
+                          children: [
+                            Container(child: Row(children: [
+                               Column(children: [Text('Real Feel'), Text('${feels.toString()}°C')],),
+                               Column(children: [Text('Wind Speed'), Text('${data.speed.toString()} km/h')],)
+                            ],
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            
+                            ),
+                            padding: EdgeInsets.all(10),
+                            ),
+                            Container(child: Row(children: [
+                              Column(children: [Text('Pressure'), Text('${pressUre.toString()} mbar')],),
+                              Column(children: [Text('Humidity'), Text('${data.humidity.toString()}%')],),
+                            ],
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            ),),
+                            Container(child: Row(children: [
+                               Column(children: [Text('Sunrise'), Text('${sunrise.toString()}')],),
+                               Column(children: [Text('Sunset'), Text('${sunset.toString()}')],)
+                            ],
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            
+                            ),
+                             padding: EdgeInsets.all(10),),
+
+                          ],
+
+
+
+                        ),)
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ));
   }
 }
