b4ba8836003ce93c4e3e89330e3e30a9ac0f2a80
pmaurya355 <82441228+pmaurya355@users.noreply.github.com>
2021-06-28 16:53:43 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:weather/loading.dart';
+import 'dart:ui' as ui;
+import 'loading.dart';
 
-import 'home_screen.dart';
-
-void main() => runApp(MyApp());
+void main() {
+  RenderErrorBox.backgroundColor = Colors.blueGrey;
+  RenderErrorBox.textStyle = ui.TextStyle(color: Colors.transparent);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
-      routes: {
-        // Home.routeName: (ctx) => Home(),
-      },
+      home: Loading(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
