b1424867e4f1e191f4320dea50516eee1fcdcda1
Ananya Agrawal <77062539+ananya704@users.noreply.github.com>
2021-06-30 00:50:09 +0530

    Update weatherData.dart

--- lib/weatherData.dart
+++ lib/weatherData.dart
@@ -16,32 +16,36 @@ class WeatherData {
   int rise;
   int set;
 
-  WeatherData({this.time, this.name, this.temp, this.stat, this.icon}); //this.name
+  WeatherData(
+      {this.time, this.name, this.temp, this.stat, this.icon}); //this.name
 
   Future<void> getData() async {
     // make a request
-    http.Response response = await http.get(
-        Uri.parse(
-          'https://api.openweathermap.org/data/2.5/weather?q=$name&appid=c2397a293bad6bf5af8760b6e4207dfb'
-        )
-    );
-    print(response.body);
+    http.Response response = await http.get(Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?q=$name&appid=c2397a293bad6bf5af8760b6e4207dfb'));
+    // print(response.body);
     Map data = jsonDecode(response.body);
 
     //Extract Data
-    time = new DateTime.fromMillisecondsSinceEpoch(data['dt'] * 1000, isUtc: false).toString();
+    time =
+        new DateTime.fromMillisecondsSinceEpoch(data['dt'] * 1000, isUtc: false)
+            .toString();
     name = data['name'];
 
     temp = data['main']['temp'].toDouble();
+
     stat = data['weather'][0]['main'];
     icon = data['weather'][0]['icon'];
 
     feel = data['main']['feels_like'].toDouble();
-    speed = data['wind']['speed'];
+
+    speed = data['wind']['speed'].toDouble();
+
     pressure = data['main']['pressure'].toDouble();
+
     humidity = data['main']['humidity'].toInt();
+
     rise = data['sys']['sunrise'];
     set = data['sys']['sunset'];
   }
 }
-
