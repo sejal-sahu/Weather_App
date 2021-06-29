eabfc67734f7479a36559c997a47825c4262d738
Ananya Agrawal <77062539+ananya704@users.noreply.github.com>
2021-06-30 00:48:59 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:weather/home_screen.dart';
-import 'package:weather/loading_screen.dart';
+import 'package:weather/loading.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'dart:ui' as ui;
-import 'loading_screen.dart';
+import 'loading.dart';
 
 void main() {
   RenderErrorBox.backgroundColor = Colors.blueGrey;
@@ -15,13 +16,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'How\'s weather',
-      theme: ThemeData(
-        primaryColor: Colors.blue.shade100,
-      ),
       home: Home(),
-      routes: {},
+      theme: ThemeData(
+        fontFamily: GoogleFonts.montserrat().fontFamily,
+      ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
-
